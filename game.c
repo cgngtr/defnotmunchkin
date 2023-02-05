670bc413ab8e005e13b62ec40acac44d040ab6b3
Reveck <cgngtr5026@hotmail.com>
2023-02-05 11:53:21 +0300

    bitti sayilir

--- game.c
+++ game.c
@@ -5,7 +5,7 @@
 
 typedef struct
 {
-    char name[20];
+    char name[16];
     int level;
     int power;
 } NPC;
@@ -13,8 +13,7 @@ typedef struct
 
 typedef struct 
 {
-    char name[20];
-    char detail[200];
+    char name[16];
     int level;
     int power;
 } CARD;
@@ -22,14 +21,16 @@ typedef struct
 
 typedef struct 
 {
-    char name[20];  
+    char name[16];  
     int power;
 } ITEM;
 
 
 
 int calculatePower(int s1, int s2);
-int playerPower, totalPowerM[20],i=0;
+int pickACard(int s1);
+int curse(int s1);
+int playerPower, totalPowerM[16],i=0;
 int main(){
 
     int r = 1; // Bu değişken, oyunun devam edip etmeme durumunu kontrol edecek.
@@ -38,7 +39,7 @@ int main(){
     srand(time(NULL));
     int temppw = 1 + rand() % 3; // değişecek güç miktarı (+-3)
 
-    int level=1,power=0;
+    int level=1,power=5;
     NPC player = {"Reveck",level,power};
 
     if(rand() % 2==0) // güç için + mı - mi kararı.
@@ -53,7 +54,6 @@ int main(){
     CARD card1 = {""};
 
     NPC monsters[] = {monster1, monster2, monster3, monster4}; 
-
     while(r<=4){
         totalPowerM[i] = calculatePower(monsters[i].level,monsters[i].power); // monster total
         playerPower = calculatePower(player.level,player.power); // player total
@@ -62,7 +62,10 @@ int main(){
         int monsterPower = totalPowerM[i];
 
         if(monsterPower>playerPower){
-            printf("You lost.\tRolling a dice...\n");
+            printf("You lost.\t");
+            if(r==4)
+                break;
+            printf("Rolling a dice...\n");
             srand(time(NULL));
             int temp = 1 + rand() % 6;
             if(temp >= 5)
@@ -74,6 +77,7 @@ int main(){
             else{
                 printf("You rolled %d\n",temp);
                 printf("\"A bad thing\" is gonna happen to you.\n"); // Sonrasında buraya bir ceza eklenecek.
+                player.level = player.level + curse(player.level);
             }
             
         }
@@ -83,7 +87,9 @@ int main(){
             printf("Do you want to draw a card?(y/n)");
             scanf("%s",&x);
             if(x=='y'){
-                
+                player.level = player.level + pickACard(player.level);
+
+
             }
             else{
 
@@ -99,4 +105,67 @@ int main(){
 int calculatePower(int s1, int s2)
 {
     return s1+s2;
+}
+
+int pickACard(int s1){
+    int random = 1 + rand() % 6;
+    s1 = 0;
+    switch(random){
+        case 1:
+            printf("You broke your leg. -1dmg.\n");
+            s1 -= 1;
+            return s1;
+            break;
+        case 2:
+            printf("You don't feel very well. -2dmg.\n");
+            s1 -= 2;
+            return s1;
+            break;
+        case 3:
+            printf("Your holy sword is broken. -3dmg.\n");
+            s1 -= 3;
+            return s1;
+            break;
+        case 4:
+            printf("Natural stones that your girlfriend bought you are working, I guess. +2dmg.\n");
+            s1 += 2;
+            return s1;
+            break;
+        case 5:
+            printf("Your amulet had a spark. +4dmg.\n");
+            s1 += 4;
+            return s1;
+            break;
+        case 6:
+            printf("They hurt your sister. +7dmg.\n");
+            s1 += 7;
+            return s1;
+            break;
+        default:
+            printf("**\n");
+    }
+}
+
+int curse(int s1){
+    int random = 1 + rand() % 3;
+    s1 = 0;
+    switch(random){
+        case 1:
+            printf("You broke your leg. -1dmg.\n");
+            s1 -= 1;
+            return s1;
+            break;
+        case 2:
+            printf("You don't feel very well. -2dmg.\n");
+            s1 -= 2;
+            return s1;
+            break;
+        case 3:
+            printf("Your holy sword is broken. -3dmg.\n");
+            s1 -= 3;
+            return s1;
+            break;
+        default:
+            printf("**\n");
+    }
 }
